346953b68f6beef0ae4e005e4c5a6f33eedfdf39
Andrew Burnes <andrew.burnes@gsa.gov>
2022-03-18 14:18:17 -0700

    Clean up image shortcodes

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const Image = require('@11ty/eleventy-img');
 
 module.exports.imageWithClassShortcode = async function imageWithClassShortcode(
@@ -5,15 +6,19 @@ module.exports.imageWithClassShortcode = async function imageWithClassShortcode(
   cls,
   alt
 ) {
-  let metadata = await Image(src, {
-    formats: ['webp', 'png', 'jpeg', 'svg'],
+  const ext = path.extname(src);
+  const fileType = ext.replace('.', '');
+
+  const metadata = await Image(src, {
+    formats: [fileType],
     outputDir: './_site/img/',
   });
 
-  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  const data = metadata[fileType] ? metadata[fileType][0] : metadata.jpeg[0];
   return `<img src="${data.url}" class="${cls}" alt="${alt}" loading="lazy" decoding="async">`;
 };
 
+
 module.exports.imageShortcode = async function imageShortcode(src, alt) {
-  return await this.imageWithClassShortcode(src, '', alt);
+  return await this.imageWithClassShortcode(src, '', alt)
 };
